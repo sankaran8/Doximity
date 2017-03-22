6559ed73075efe20d91c89a64c69407a0519b2ad
Sankaran P S <sankaran.ps@gmail.com>
2017-03-22 23:03:23 +0530

    Licence, composer.jsone style.yml

---
+++ src/Provider.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace SocialiteProviders\Doximity;
+
+use Laravel\Socialite\Two\AbstractProvider;
+use Laravel\Socialite\Two\ProviderInterface;
+use Laravel\Socialite\Two\User;
+
+class Provider extends AbstractProvider implements ProviderInterface
+{
+    /**
+     * The scopes being requested.
+     *
+     * @var array
+     */
+    protected $scopes = ['basic', 'email'];
+
+    /**
+     * The separating character for the requested scopes.
+     *
+     * @var string
+     */
+    protected $scopeSeparator = ' ';
+
+    /**
+     * Get the authentication URL for the provider.
+     *
+     * @param  string  $state
+     * @return string
+     */
+    protected function getAuthUrl($state)
+    {
+        return $this->buildAuthUrlFromBase(
+            'https://auth.doximity.com/oauth/authorize',
+            $state
+        );
+    }
+
+    /**
+     * Get the token URL for the provider.
+     *
+     * @return string
+     */
+    protected function getTokenUrl()
+    {
+        return 'https://auth.doximity.com/oauth/token';
+    }
+
+    /**
+     * Get the raw user for the given access token.
+     *
+     * @param  string  $token
+     * @return array
+     */
+    protected function getUserByToken($token)
+    {
+        $response = $this->getHttpClient()->get(
+            'ttps://doximity.com/api/v1/users/current',
+            [
+                'headers' => [
+                    'Authorization' => 'Bearer '.$token,
+                ],
+            ]
+        );
+
+        return json_decode($response->getBody(), true);
+    }
+
+    /**
+     * Map the raw user array to a Socialite User instance.
+     *
+     * @param  array  $user
+     * @return \Laravel\Socialite\Two\User
+     */
+    protected function mapUserToObject(array $user)
+    {
+        return (new User())->setRaw($user)->map([
+            'id'            => $user['id'],
+            'nickname'      => $user['username'],
+            'name'          => $user['full_name'],
+            'email'         => $user['email'],
+            'first_name'    => $user['first_name'],
+            'middle_name'   => $user['middle_name'],
+            'last_name'     => $user['last_name'],
+            'avatar'        => $user['profile_photo'],
+        ]);
+    }
+
+    /**
+     * Get the POST fields for the token request.
+     *
+     * @param  string  $code
+     * @return array
+     */
+    protected function getTokenFields($code)
+    {
+        return array_merge(parent::getTokenFields($code), [
+            'grant_type' => 'authorization_code'
+        ]);
+    }
+}
